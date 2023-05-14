793aa21b4e07b0c740ce6fd6800a49c35e789777
Igor Cerutti Lima <igor.cpv2@gmail.com>
2023-05-14 06:28:47 -0300

    Projeto

---
+++ Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Blog.Services;
+
+public class EmailService
+{
+    public bool Send(
+        string toName,
+        string toEmail,
+        string subJect,
+        string body,
+        string fromName = "Equipe Igor",
+        string fromEmail = "igor.cpv2@gmail")
+    {
+        var smtpClient = new SmtpClient(Configuration.Smtp.Host, Configuration.Smtp.Port);
+        
+        smtpClient.Credentials = new NetworkCredential(Configuration.Smtp.UserName, Configuration.Smtp.Password);
+        smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        smtpClient.EnableSsl = true;
+
+        var mail = new MailMessage();
+        mail.From = new MailAddress(fromEmail, fromName);
+        mail.To.Add(new MailAddress(toEmail, toName));
+        mail.Subject = subJect;
+        mail.Body = body;
+        mail.IsBodyHtml = true;
+
+        try
+        {
+            smtpClient.Send(mail);
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+}
---
+++ ViewModels/Accounts/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels.Accounts;
+
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    [StringLength(70), MinLength(3, ErrorMessage = "O nome deve ser entre 3 e 70 caracteres")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "O Email é obrigatório")]
+    [EmailAddress(ErrorMessage = "O Email é inválido")]
+    public string Email { get; set; }
+    
+}
